e871ceef3fab7d8dddc647dd9ec83ff4f6b0e9b4
AP Java Period 5 <apjavaperiod5@ac1mwk0r3jc9h.78b505.nycboe.org>
2022-10-17 14:22:54 -0400

    finished favorite movie

---
+++ 02 - Favorite Movie/favoritemovie.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class favoritemovie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class favoritemovie
+{
+    public static void main(String[] args) {
+        System.out.println("The movie is about an unlucky assassinwho is tasked a job in a bullet train.");    
+        System.out.println("But there are also other assassins in that train, each with their own separate tasks");      
+        System.out.println("And during the main character doing his own mission, they all manage to get in each others way, but the main villain, the White Death, is waiting for all of them ");      
+        System.out.println("So at the end, they all join forces to fight the White Death and his goons.");
+    }
+    
+}
